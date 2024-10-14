d4438dc00454133830e308651bf368733cb32bb1
nathsant <nath091santos@gmail.com>
2024-10-14 12:13:19 -0300

    implementando atributos

--- src/main/java/br/sesi/bank/bank_java_jdbc/domain/cliente/Cliente.java
+++ src/main/java/br/sesi/bank/bank_java_jdbc/domain/cliente/Cliente.java
@@ -1,5 +1,7 @@
 package br.sesi.bank.bank_java_jdbc.domain.cliente;
 
+import java.util.Objects;
+
 public class Cliente {
     private String nome;
     private String email;
@@ -10,7 +12,6 @@ public class Cliente {
         this.email = dados.email;
         this.cpf = dados.cpf;
     }
-
     public String getEmail() {
         return email;
     }
@@ -19,5 +20,25 @@ public class Cliente {
     }
     public String getCpf() {
         return cpf;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return cpf.equals(cliente.cpf);
+    }
+    @Override
+    public int hashCode(){
+        return Objects.hash(cpf);
+    }
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nome='" + nome +'\'' +
+                ", cpf='" + cpf + '\'' +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
--- src/main/java/br/sesi/bank/bank_java_jdbc/domain/conta/Conta.java
+++ src/main/java/br/sesi/bank/bank_java_jdbc/domain/conta/Conta.java
@@ -3,33 +3,50 @@ package br.sesi.bank.bank_java_jdbc.domain.conta;
 import br.sesi.bank.bank_java_jdbc.domain.cliente.Cliente;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Conta {
     private Integer numero;
-    private BigDecimal Valor;
+    private BigDecimal saldo;
     private Cliente titular;
 
-    public Conta(Integer numero, BigDecimal Valor, Cliente titular){
+    public Conta(Integer numero, BigDecimal saldo, Cliente titular){
         this.numero = numero;
-        this.Valor = Valor;
+        this.saldo = saldo;
         this.titular = titular;
     }
     public boolean possuiSaldo(){
-        return true;
+        return this.saldo.compareTo(BigDecimal.ZERO) != 0;
     }
     public void sacar(BigDecimal valor){
-
+        this.saldo = this.saldo.subtract(valor);
     }
     public void depositar(BigDecimal valor){
-
+        this.saldo = this.saldo.add(valor);
     }
     public Integer getNumero(){
         return numero;
     }
     public BigDecimal getSaldo(){
-        return Valor;
+        return saldo;
     }
     public Cliente getTitular(){
         return titular;
     }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numero);
+    }
+
+    @Override
+    public String toString(){
+        return "Conta{" +
+                "numero'" + numero + '\'' +
+                ", saldo=" + saldo +
+                ", titular=" + titular +
+                '}';
+    }
+
+
 }
